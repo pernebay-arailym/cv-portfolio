1def3dda267456f0c61d6e1d41ed6b7511b04d28
pernebay-arailym <pernebayarailym@gmail.com>
2023-11-19 12:58:37 +0100

    :wrench: config svg, ts packages :package:

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginRequire from 'vite-plugin-require';
+import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgLoader(), vitePluginRequire.default({ fileRegex:/(.jsx?|.tsx?|.svg)$/ })],
 })

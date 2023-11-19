f935a9742c580050790fa40631817b66fa3c6a07
pernebay-arailym <pernebayarailym@gmail.com>
2023-11-19 12:59:51 +0100

    add footer and header

---
+++ src/PortfolioContainer/Home/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer-parent">
+        <img
+          src={require("../../../assets/Home/shape-bg.png?url")}
+          alt="not internet connection"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

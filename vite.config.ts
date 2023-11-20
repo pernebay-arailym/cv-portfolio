cf35b30428e1eb901267227fb38b557721eea6c3
pernebay-arailym <pernebayarailym@gmail.com>
2023-11-20 21:42:57 +0100

    put back svg config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import vitePluginRequire from 'vite-plugin-require';
+import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/cv-portfolio/",
-  plugins: [react(), vitePluginRequire.default({ fileRegex:/(.jsx?|.tsx)$/ })],
+  plugins: [react(), svgLoader(), vitePluginRequire.default({ fileRegex:/(.jsx?|.tsx?|.svg)$/ })],
 })

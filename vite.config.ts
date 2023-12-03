73fc3d16b1bc6ee0bc520a5ed206c156771cb0cf
chicoumbetov <shyngysmaaan@gmail.com>
2023-12-03 22:51:12 +0100

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import vitePluginRequire from 'vite-plugin-require';
-import svgLoader from 'vite-svg-loader';
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+// import vitePluginRequire from 'vite-plugin-require';
+// import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cv-portfolio/",
-  plugins: [react(), svgLoader(), vitePluginRequire.default({ fileRegex:/(.jsx?|.tsx?|.svg)$/ })],
-})
+  base: "./",
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.tsx"),
+      name: "cv-portfolio",
+      fileName: "cv-portfolio",
+    },
+    outDir: "./dist",
+    target: "esnext", // transpile as little as possible
+  },
+  plugins: [dts({ insertTypesEntry: true }), react()],
+  // plugins: [react()], // , svgLoader(), vitePluginRequire.default({ fileRegex:/(.jsx?|.tsx?|.svg)$/ })],
+});
